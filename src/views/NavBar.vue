d74492edcb3c13b06dbe6f6dac47d78c47f1d8bd
WaiLinnHtetAung <wailinhtetaung007@gmail.com>
2023-05-01 22:34:54 +0630

    new commit for aws testng

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main-nav">
     <router-link to="/">Projects</router-link>
-    <router-link to="/addProject">Add Project</router-link>
+    <router-link to="/addProject">Add Projects</router-link>
   </div>
 </template>
 
